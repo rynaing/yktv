855d78612f5463d540afc809ba59f299d902432a
rynaing <ry.naing@gmail.com>
2023-06-09 10:00:23 -0400

    ryan's changes

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-logo-container">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          yerrr
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+         // href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn hydroponic
         </a>
       </header>
     </div>
